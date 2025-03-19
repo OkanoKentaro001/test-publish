de96c504ecc163ae45cd3e49a63dee49172a7bde
OkanoKentaro001 <okano.kentaro.001@gmail.com>
2025-03-19 18:39:52 +0900

    lerna導入

---
+++ packages/package-a/index.js
@@ -0,0 +1,7 @@
+// packages/package-a/index.js
+
+const exampleFunction = () => {
+  console.log('This is package-a');
+};
+
+module.exports = { exampleFunction };
---
+++ packages/package-b/index.js
@@ -0,0 +1,7 @@
+// packages/package-b/index.js
+
+const exampleFunction = () => {
+  console.log('This is package-b');
+};
+
+module.exports = { exampleFunction };
